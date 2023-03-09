fdcafa8cc5d3b84a45e21534186bbbe2b9ecf32e
Richard Borges <richard.borges.cc@gmail.com>
2023-03-09 09:21:48 -0300

    chore: handling auth form data

---
+++ lib/models/auth_form_data.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+enum AuthMode {
+  signup,
+  login,
+}
+
+class AuthFormData {
+  String name = '';
+  String email = '';
+  String password = '';
+  File? image;
+  AuthMode _mode = AuthMode.login;
+
+  bool get isLogin {
+    return _mode == AuthMode.login;
+  }
+
+  bool get isSignup {
+    return _mode == AuthMode.signup;
+  }
+
+  void togleAuthMode() {
+    _mode = isLogin ? AuthMode.signup : AuthMode.login;
+  }
+}
