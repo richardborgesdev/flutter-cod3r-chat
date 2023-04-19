df8b48f4114f276d5c0a8f60fa0b60b100f3d18c
Richard Borges <richard.borges.cc@gmail.com>
2023-04-19 14:45:31 -0300

    chore: start chat page UI

---
+++ lib/components/new_message.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatelessWidget {
+  const NewMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
