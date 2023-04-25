5480f9b592c01c1f2f4a964c1df5752bfbf04dca
Richard Borges <richard.borges.cc@gmail.com>
2023-04-25 11:28:38 -0300

    chore: handle chat connection state

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cod3r_chat/core/models/chat_message.dart';
+import 'package:flutter_cod3r_chat/core/services/chat/chat_service.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder<List<ChatMessage>>(
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData) {
+          return Center(
+            child: Text('Sem dados. Vamos conversar?'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+
+          return ListView.builder(
+            itemBuilder: (context, index) => Text(msgs[index].text),
+            itemCount: msgs.length,
+          );
+        }
+      },
+      stream: ChatService().messagesStream(),
+    );
   }
 }
